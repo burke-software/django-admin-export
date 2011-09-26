b4735b7b9a14723f2281ce7275027365ff1eecbb
David Burke <david@burkesoftware.com>
2011-09-26 12:44:49 -0400

    Getting ready to submit to pypi

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "django-admin-export",
+    version = "1.0",
+    author = "David Burke",
+    author_email = "david@burkesoftware.com",
+    description = ("Generic export to xls action for Django admin interface"),
+    license = "BSD",
+    keywords = "django admin",
+    url = "http://code.google.com/p/django-admin-export/",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        "License :: OSI Approved :: BSD License",
+    ],
+)
